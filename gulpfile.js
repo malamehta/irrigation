fbaffcb6893db8d44add41cd0ffae11337d91c70
malamehta <malamehta370@gmail.com>
2022-02-04 01:29:07 +0530

    gulpfile added

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass')(require('sass'));
+
+gulp.task('serve',function(){
+    browserSync.init({
+        server:"./app"
+    });
+    gulp.watch("app/scss/*.scss",gulp.series('sass'));
+    gulp.watch("app/*.html").on('change',browserSync.reload);
+});
+
+gulp.task('sass',function(){
+    return gulp.src("app/scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("app/styles"))
+    .pipe(browserSync.stream());
+
+
+});
+
+gulp.task('start',gulp.series('serve','sass'));
